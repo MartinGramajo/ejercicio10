f5459c3dec27dc0447412232c27c99fdbd0c99c1
¨mago¨ <martingramajo08@gmail.com>
2023-07-22 01:06:02 -0300

    agrego estados y local storage en app y llamo FormularioPeliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import FormularioPeliculas from "./components/FormularioPeliculas";
 
 function App() {
+  let peliculasIniciales = JSON.parse(localStorage.getItem("peliculas"));
+  if (!peliculasIniciales) {
+    peliculasIniciales = [];
+  }
+  const [peliculas, setPeliculas] = useState(peliculasIniciales);
+
+  useEffect(() => {
+    if (peliculasIniciales) {
+      localStorage.setItem("peliculas", JSON.stringify(peliculas));
+    } else {
+      localStorage.setItem("peliculas", JSON.stringify([]));
+    }
+  }, [peliculas]);
+
+  const nuevaPelicula = (pelicula) => {
+    setPeliculas([...peliculas, pelicula]);
+  };
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="bg-app d-flex flex-column min-vh-100">
+      <section>
+        <h1 className="text-center py-5 display-6">
+          Administrador de Películas
+        </h1>
+      </section>
+      <section>
+        <FormularioPeliculas nuevaPelicula={nuevaPelicula} />
+      </section>
+      <section></section>
+      <footer className="py-5 mt-auto bg-dark">
+        <h5 className="text-center text-white">
+          {" "}
+          © Todos los derechos reservados
+        </h5>
+      </footer>
+    </div>
   );
 }
 
